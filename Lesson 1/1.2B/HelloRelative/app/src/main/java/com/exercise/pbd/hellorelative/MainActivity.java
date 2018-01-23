3cec289471524bf95ff8e018e0ec718cfaf8a671
annisanurulazhar <annisanurulazhar@yahoo.com>
2018-01-23 15:12:46 +0700

    Add HelloRelative project

---
+++ Lesson 1/1.2B/HelloRelative/app/src/main/java/com/exercise/pbd/hellorelative/MainActivity.java
@@ -0,0 +1,30 @@
+package com.exercise.pbd.hellorelative;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int initial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initial = 0;
+    }
+
+    public void showToast (View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello, World!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp (View view) {
+        initial++;
+        TextView number = (TextView) findViewById(R.id.show_count);
+        number.setText(Integer.toString(initial));
+    }
+}
